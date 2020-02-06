d8286206b92f0876bddbe5051104f0eda0097f6a
valentinjacot <vjd.jako@gmail.com>
2020-02-06 09:13:36 +0100

    200206 VJD graviApp functionning version

--- GraviApp/app/src/main/java/com/example/graviapp/MainActivity.java
+++ GraviApp/app/src/main/java/com/example/graviapp/MainActivity.java
@@ -8,7 +8,11 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,19 +30,35 @@ public class MainActivity extends AppCompatActivity {
     public int getWeeks(Date date){
         return 0;
     }
-    public void startCalculation(View view){
-        Intent intent = new Intent( this, MainActivity.class); //args: one context (this) and a class
+    public void startCalculation(View view)throws Exception{
+        //Intent intent = new Intent( this, MainActivity.class); //args: one context (this) and a class
         EditText editText = (EditText) findViewById(R.id.editText);
-        String term = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, term); //adds the value of EditText to the intent
-        //startActivity(intent); // starts an instance of the DisplayMessageActivity (spec. by the intent)
+        String termString = editText.getText().toString();
+        /************************************/
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar today = Calendar.getInstance();
+        Date termDate;
+        try {
+            termDate = sdf.parse(termString);
+        } catch (ParseException e){
+            throw e;
+        }
+        Calendar termCal = Calendar.getInstance();
+        termCal.setTime(termDate);
+        int res =280 - (int) dateComputing.getDifferenceDays(today, termCal);
+        //startActivity(intent); // starts an instance of the DisplayMessageActivity (spec. by the intent)$
+        int numWeek = (int) Math.ceil(res/7);
+        int numDay = (int) res % 7;
+        /************************************/
+
+        //intent.putExtra(EXTRA_MESSAGE, res); //adds the value of EditText to the intent
+        TextView textView = findViewById(R.id.textView3);
+
+        textView.setText(Integer.toString(numWeek) + " Weeks and " + Integer.toString(numDay)+ " Days");
     }
-    public void displayDate(View view){
+/**    public void displayDate(View view){
         Intent intent =  getIntent();
         String term = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView textView = findViewById(R.id.textView3);
-        textView.setText(term);
-
-    }
+    }**/
 
 }
---
+++ GraviApp/app/src/main/java/com/example/graviapp/dateComputing.java
@@ -0,0 +1,13 @@
+package com.example.graviapp;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+
+public class dateComputing {
+    public static long getDifferenceDays(Calendar d1,Calendar d2) {
+        long diff = d2.getTime().getTime() - d1.getTime().getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+}
